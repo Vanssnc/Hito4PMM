b86a791f72e1434ddc170210ffe91284a9945293
Vanessa Nicole <vanessa.ncore@gmail.com>
2024-10-14 18:32:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./components/Home/Home"
+import Footer from "./components/Footer/Footer"
+import  Register from "./components/RegisterPage/Register"
+import Login from "./components/LoginPage/Login1"
+import Cart from "./components/Cart/cart"
+import Pizza from "./components/pizza-h4/Pizza"
+
+function App() {
+ 
+  return (
+    <>
+    <Navbar/>
+    {/* <Home/> */}
+    <Pizza/>
+    <Footer/>
+    {/* <Register/> */}
+    {/* <Login/> */}
+    {/* <Cart/> */}
+    </>
+  )
+}
+
+export default App
